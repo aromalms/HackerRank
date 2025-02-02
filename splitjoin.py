453913ef9dc332d69140a06b50bd6c09b0bb7703
Aromal M S <aromalms3318@gmail.com>
2025-02-02 14:40:43 +0530

    commit

---
+++ splitjoin.py
@@ -0,0 +1,9 @@
+#string split and join
+def split_and_join(line):
+    list=line.split(" ")
+    list="-".join(list)
+    return list
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
